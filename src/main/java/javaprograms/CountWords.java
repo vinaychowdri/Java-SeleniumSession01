39f57c36bee7c68a3f317f133f034e289eaa214d
vinay <vinaychowdri48@gmail.com>
2022-09-24 21:27:29 +0530

    CountWordProgram

--- src/main/java/javaprograms/CountWords.java
+++ src/main/java/javaprograms/CountWords.java
@@ -1,12 +1,14 @@
 package javaprograms;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountWords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/*	// TODO Auto-generated method stub
 		
 		String str="i am learning java java program for qa interview";
 		
@@ -24,7 +26,28 @@ public class CountWords {
 		{
 			System.out.println("each word count:"+x+"="+map.get(x));
 		}
+		*/
+	
+	
+	//String str="vinay learning java java program ";
+	Scanner scr=new Scanner(System.in);
+	String str=scr.nextLine();
+	Map<String,Integer> map=new HashMap<String,Integer>();
+	Integer Count=1;
+	String[] arr=str.split(" ");
+	for (int i=0;i<arr.length;i++) {
+		if (!map.containsKey(arr[i])) {
+			map.put(arr[i], Count);
+			
+		}else {
+			map.put(arr[i], map.get(arr[i])+1);
+		}
 		
+		
+	}
+	for(String x:map.keySet()) {
+		System.out.println("Each word count:"+x+"="+map.get(x));
+	}
 	}
 
 }
